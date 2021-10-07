970bf6ea65a15b1cf2892eb7b50d0b1d421c4a23
Dalvin <Dalvin.sejour@dutchie.com>
2021-10-07 14:59:42 -0700

    add mocks for tracing and finish

---
+++ test/tracing_test.dart
@@ -0,0 +1,46 @@
+import 'package:flutter_test/flutter_test.dart';
+import 'package:mockito/annotations.dart';
+import 'package:datadog_flutter/datadog_tracing.dart';
+import 'package:datadog_flutter/src/channel.dart';
+
+@GenerateMocks([DatadogTracing])
+
+void main() {
+  TestWidgetsFlutterBinding.ensureInitialized();
+  final tester = TestDefaultBinaryMessengerBinding.instance!.defaultBinaryMessenger;
+
+  final Future<Map<String, String>> map = Future.value({
+    "resourceName": "greenbits", "method": "get", "url": "http://greenbits.com"
+  });
+
+  tester.setMockMethodCallHandler(channel,
+    (methodCall) async {
+      if (methodCall.method == 'tracingCreateHeadersForRequest') {
+        return map;
+      }
+
+      if (methodCall.method == "tracingFinishSpan") {
+        return <String, String>{};
+      }
+  });
+
+
+
+  group("DatadogTracing", (){
+    const spanId = "test-test";
+    group('#finishSpan', (){
+      test("tracing finished", () async {
+        await DatadogTracing.initialize();
+        final httpClient =  DatadogTracingHttpClient();
+        final response = await httpClient.get(Uri(path: 'http://greenbits.com'));
+        expect(() => DatadogTracing.finishSpan(spanId, statusCode: response.statusCode), returnsNormally);
+      });
+    });
+
+    group('#createHeaders', (){
+      test("create headers", () async {
+        expect(() => DatadogTracing.createHeaders(resourceName: "greenbits", method: "get", url: "http://greenbits.com"), returnsNormally);
+      });
+    });
+  });
+}
